9dff9dd4ca35e25e870aa31de1e5e975185dd650
Marcin Siemieniuk-Morawski <msiemieniukmorawski@gmail.com>
2026-09-08 09:41:58 +0200

    Bump version to 2.3.0

--- github-theme-updater.php
+++ github-theme-updater.php
@@ -3,7 +3,7 @@
  * Plugin Name:       GitHub Theme Updater
  * Plugin URI:        https://github.com/msiemieniukmorawski/github-theme-updater
  * Description:       Updates a WordPress theme straight from a GitHub repository, private ones included, with backups, version rollback and per-path protection against overwriting.
- * Version:           2.2.0
+ * Version:           2.3.0
  * Requires at least: 5.8
  * Requires PHP:      7.4
  * Author:            ms-m.pl
@@ -21,7 +21,7 @@ namespace MSM\GitHubThemeUpdater;
 
 defined( 'ABSPATH' ) || exit;
 
-const VERSION     = '2.2.0';
+const VERSION     = '2.3.0';
 const PLUGIN_FILE = __FILE__;
 
 define( __NAMESPACE__ . '\PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
